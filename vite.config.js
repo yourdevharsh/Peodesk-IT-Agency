4f8f7494522d1a5d059fe4c6bbf54214876fc28d
Harsh <yourdevharsh@gmail.com>
2026-04-20 10:59:56 +0530

    Fixed CSS issue.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    viteSingleFile(),
   ],
-  build: {
-    assetsInlineLimit: 10240, 
-  },
 });
